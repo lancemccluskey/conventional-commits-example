37e411fe06dc56952c46a3b889e81bd925872240
Lance <lance.mccluskey@volusion.com>
2021-04-21 07:54:38 -0500

    feat(robot): [PROJ-3] Implementing robot service

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ server.get('/foo', async (request, reply) => {
   return 'bar\n';
 });
 
+server.get('/robot', async (request, reply) => {
+  return 'beep boop\n';
+});
+
 server.listen(3000, (err, address) => {
   if (err) {
     console.error(err);
